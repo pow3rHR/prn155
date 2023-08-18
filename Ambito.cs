c869b6965e79960b0b27927b7cbc1e5e229a37c3
Diego Herrera <psystingod@gmail.com>
2023-08-18 05:20:46 -0600

    lecture 3 scope and intro to objects added

---
+++ Ambito.cs
@@ -0,0 +1,14 @@
+string nombre = "Pepito";
+
+string nombre_mayusculas() {
+    return nombre.ToUpper();
+}
+
+Console.WriteLine(nombre_mayusculas());
+
+void variable_local() {
+    int numero = 25;
+    Console.WriteLine(numero);
+}
+
+//Console.WriteLine(numero);
