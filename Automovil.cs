2531bf4df664d33c83e463b659f1e6f8e96383d1
Diego Herrera <psystingod@gmail.com>
2023-08-23 08:03:05 -0600

    encapsulation was added

---
+++ Automovil.cs
@@ -0,0 +1,17 @@
+class Automovil {
+    public int nPuertas;
+    public string color;
+    public string marca;
+    private string nSerie;
+    private int hp = 110;
+    private double aceleracion = 0.0;
+
+    private void acelerar() {
+
+    }
+
+    public void pisarAcelerador() {
+
+    }
+
+}
