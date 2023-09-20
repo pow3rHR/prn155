f2aa25ea1fec98adde42f5d5d8c9aea194edbd7d
Diego Herrera <psystingod@gmail.com>
2023-09-20 07:31:11 -0600

    getters and setters was added

--- 3-GetterSetter.cs
+++ 3-GetterSetter.cs
@@ -2,6 +2,12 @@ class Automovil
 {
     public string marca { 
         get { return "BMW"; }
+
+        set {
+            if(value == "Toyota") {
+                Console.WriteLine("Utilice una marca distinta");
+            }
+        } 
     }
 }
 
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ class Program
     static void Main(string[] args)
     {
         Automovil a = new Automovil();
+        a.marca = "Toyota";
         Console.WriteLine(a.marca);
     }
 }
