aaf83dd456e39f89a6a00afa965045c15c569b0e
Diego Herrera <psystingod@gmail.com>
2023-10-19 20:50:44 -0600

    Agregado manejo de strings

--- Program.cs
+++ Program.cs
@@ -3,6 +3,12 @@ class Program
 {
     static void Main(string[] args)
     {
+        string[] mi_cadena = {"Soy", "estudiante", "de", "la", "UES"};
+
+        Console.WriteLine(string.Concat(mi_cadena));
+        Console.WriteLine(string.Join(" ", mi_cadena));
+
+        string mi_otra_cadena = "Soy estudiante de la UES y no tiro piedras.";
         
     }
 }
